93d36153675ca5fdd3f537299e5046f0a7254d6e
Anastasia999 <anastasiya.leonovich@mail.ru>
2020-08-12 12:43:49 +0700

    change language parameter

--- conftest.py
+++ conftest.py
@@ -1,25 +1,19 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default="en-gb")
+    parser.addoption('--language', action='store', default="en")
 
-@pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_guest_should_see_login_link(browser, language):
-    from selenium.webdriver.chrome.options import Options
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
-    browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
